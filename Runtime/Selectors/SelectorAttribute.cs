9895c0f20db3da88c78e3f3deac25af49245dbfd
Kira Dinan <kira.dinan@fallingsnowinteractive.ca>
2025-08-06 20:51:21 -0300

    Selector attribute

---
+++ Runtime/Selectors/SelectorAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace Fsi.Ui.Selectors
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SelectorAttribute : PropertyAttribute
+    {
+        
+    }
+}
